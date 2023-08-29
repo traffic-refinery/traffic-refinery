e92f6d4c4bf880c057b1bd560060f54fa88a8108
Francesco Bronzino <wontoniii@gmail.com>
2023-08-29 16:49:22 +0200

    Small fixes

--- scripts/run_replay.sh
+++ scripts/run_replay.sh
@@ -27,12 +27,14 @@ pid=$!
 #Do tcp replay
 echo "Starting replay for trace $trace"
 tcpreplay -i lo $trace
+echo "Concluded replay"
 
 #kill netm
 echo "Killing process $pid"
 kill -2 $pid
 
-folder=/tmp/ta.*.out
+folder=/tmp/tr.*.out
+ls /tmp/
 for f in $folder; do
     echo "Copying file $f to /out/"
     mv $f /out/
